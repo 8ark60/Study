bf22d518699869a1bafbedb16aab239068706592
8ark60 <8ark6young@gmail.com>
2022-08-29 17:04:09 +0900

    tab

---
+++ JS/Step10/tab.js
@@ -0,0 +1,37 @@
+// 버튼을 누르면 orange class 부착 기존에 붙어있던 클래스는 제거
+// show 클래스명 부착
+
+// for 문
+
+for(var i = 0; i < 3; i++){
+    
+}
+
+
+document.querySelectorAll('.tab-button')[0].addEventListener('click', function(){
+    document.querySelectorAll('.tab-button')[0].classList.add('orange');
+    document.querySelectorAll('.tab-button')[1].classList.remove('orange');
+    document.querySelectorAll('.tab-button')[2].classList.remove('orange');
+    document.querySelectorAll('.tab-content')[0].classList.add('show');
+    document.querySelectorAll('.tab-content')[1].classList.remove('show');
+    document.querySelectorAll('.tab-content')[2].classList.remove('show');
+});
+
+document.querySelectorAll('.tab-button')[1].addEventListener('click', function(){
+    document.querySelectorAll('.tab-button')[0].classList.remove('orange');
+    document.querySelectorAll('.tab-button')[1].classList.add('orange');
+    document.querySelectorAll('.tab-button')[2].classList.remove('orange');
+    document.querySelectorAll('.tab-content')[0].classList.remove('show');
+    document.querySelectorAll('.tab-content')[1].classList.add('show');
+    document.querySelectorAll('.tab-content')[2].classList.remove('show');
+    
+});
+
+document.querySelectorAll('.tab-button')[2].addEventListener('click', function(){
+    document.querySelectorAll('.tab-button')[0].classList.remove('orange');
+    document.querySelectorAll('.tab-button')[1].classList.remove('orange');
+    document.querySelectorAll('.tab-button')[2].classList.add('orange');
+    document.querySelectorAll('.tab-content')[0].classList.remove('show');
+    document.querySelectorAll('.tab-content')[1].classList.remove('show');
+    document.querySelectorAll('.tab-content')[2].classList.add('show');
+});
